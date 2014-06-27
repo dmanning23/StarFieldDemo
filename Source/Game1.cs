d77e19f53efaeef6035a5a3bdccc99f74e948cfe
unknown <dmanning23@hotmail.com>
2014-06-27 17:19:31 -0500

    got this working

--- Source/Game1.cs
+++ Source/Game1.cs
@@ -1,79 +1,127 @@
 using Microsoft.Xna.Framework;
+using StarField;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using ResolutionBuddy;
 
-namespace StarFieldDemo.Windows
+namespace StarFieldDemo
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
-    public class Game1 : Game
-    {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Stars field;
 
-        public Game1()
-        {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-        }
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft;
+			Resolution.Init(ref graphics);
+			Content.RootDirectory = "Content";
+		}
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content. Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// Change Virtual Resolution
+			Resolution.SetDesiredResolution(1280, 720);
 
-            base.Initialize();
-        }
+			//set the desired resolution
+			Resolution.SetScreenResolution(1024, 768, false);
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
-        protected override void LoadContent()
-        {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+			// TODO: Add your initialization logic here
+			base.Initialize();
+		}
 
-            // TODO: use this.Content to load your game content here
-        }
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
+			//TODO: use this.Content to load your game content here
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
-            // TODO: Add your update logic here
+			field = new Stars(GraphicsDevice, Resolution.TitleSafeArea);
+		}
 
-            base.Update(gameTime);
-        }
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			// For Mobile devices, this logic will close the Game when the Back button is pressed
+			// Allows the game to exit
+			if ((GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed) ||
+			Keyboard.GetState().IsKeyDown(Keys.Escape))
+			{
+				this.Exit();
+			}
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+			// TODO: Add your update logic here
 
-            // TODO: Add your drawing code here
+			const float scrollSpeed = 10.0f;
 
-            base.Draw(gameTime);
-        }
-    }
+			Vector2 vel = Vector2.Zero;
+			if (Keyboard.GetState().IsKeyDown(Keys.Up))
+			{
+				vel.Y += scrollSpeed;
+			}
+			if (Keyboard.GetState().IsKeyDown(Keys.Down))
+			{
+				vel.Y -= scrollSpeed;
+			}
+			if (Keyboard.GetState().IsKeyDown(Keys.Left))
+			{
+				vel.X += scrollSpeed;
+			}
+			if (Keyboard.GetState().IsKeyDown(Keys.Right))
+			{
+				vel.X -= scrollSpeed;
+			}
+
+			field.Update(vel, Resolution.TitleSafeArea);
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			// Clear to Black
+			graphics.GraphicsDevice.Clear(Color.DarkBlue);
+
+			// Calculate Proper Viewport according to Aspect Ratio
+			Resolution.ResetViewport();
+
+			spriteBatch.Begin(SpriteSortMode.Immediate,
+			BlendState.NonPremultiplied,
+			null, null, null, null,
+			Resolution.TransformationMatrix());
+
+			field.Render(spriteBatch);
+
+			//prim.Rectangle(Resolution.TitleSafeArea, Color.Red);
+
+			spriteBatch.End();
+
+			// The real drawing happens inside the screen manager component.
+			base.Draw(gameTime);
+		}
+	}
 }
